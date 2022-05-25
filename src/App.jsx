56843afda989254f19f90f4fd231fb0970a85c98
debangi <debangiofficial@gmail.com>
2022-05-26 00:52:05 +0530

    Creating and adding chatHeader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
 
 import './App.css';
-import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className='app'>
       <Sidebar />
-      {/* chat */}
+      <Chat />
     </div>
   );
 }
---
+++ src/components/ChatHeader.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import EditLocationAltRoundedIcon from '@mui/icons-material/EditLocationAltRounded';
+import PeopleAltRoundedIcon from '@mui/icons-material/PeopleAltRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
+import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+
+import './ChatHeader.css';
+
+const ChatHeader = () => {
+  return (
+    <div className='chatHeader'>
+      <div className='chatHeader__left'>
+        <h3>
+          <span className='chatHeader__hash'>#</span>
+          Test channel name
+        </h3>
+      </div>
+
+      <div className='chatHeader__right'>
+        <NotificationsIcon />
+        <EditLocationAltRoundedIcon />
+        <PeopleAltRoundedIcon />
+
+        <div className='chatHeader__search'>
+          <input placeholder='search...' />
+          <SearchRoundedIcon />
+        </div>
+        <SendRoundedIcon />
+        <HelpRoundedIcon />
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
