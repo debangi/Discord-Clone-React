6ded3bc2acb7dd47233bf39d431b20a5ffb84555
debangi <debangiofficial@gmail.com>
2022-05-26 01:07:43 +0530

    Adding chat input form in chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import './Chat.css';
 import ChatHeader from './ChatHeader';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import CardGiftcardRoundedIcon from '@mui/icons-material/CardGiftcardRounded';
+import GifRoundedIcon from '@mui/icons-material/GifRounded';
+import InsertEmoticonRoundedIcon from '@mui/icons-material/InsertEmoticonRounded';
+import './Chat.css';
 
 const Chat = () => {
   return (
     <div className='chat'>
       <ChatHeader />
+      <div className='chat__messages'></div>
+      <div className='chat__input'>
+        <AddCircleRoundedIcon fontSize='large' />
+        <form>
+          <input placeholder={`Message #TESTCHANNEL`} />
+          <button className='chat__inputButton' type='submit'>
+            Send
+          </button>
+        </form>
+        <div className='chat__inputIcons'>
+          <CardGiftcardRoundedIcon />
+          <GifRoundedIcon />
+          <InsertEmoticonRoundedIcon />
+        </div>
+      </div>
     </div>
   );
 };
